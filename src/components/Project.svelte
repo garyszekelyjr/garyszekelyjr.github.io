83dd36ec64e9ad356cfd232b5b12ba7be31d27b0
Gary Szekely <gary.szekely.jr@gmail.com>
2024-11-21 12:40:10 -0500

    underlining of project links

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div class="mx-2 my-3">
-    <a href={link} target="_blank" class="text-white">{name}</a>
+    <a href={link} target="_blank" class="text-white hover:underline">{name}</a>
     <div class="text-white mb-2">{description}</div>
     {#each badges as badge}
         <span class="mr-1 border rounded p-1 text-white">{badge}</span>
