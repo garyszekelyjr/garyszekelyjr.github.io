28619c6c53d0f980824da315f060eaf0c544cfb8
Gary Szekely <gary.szekely.jr@gmail.com>
2024-12-12 23:09:03 -0500

    changes to scraper

--- src/main.py
+++ src/main.py
@@ -3,57 +3,86 @@ import json
 from pathlib import Path
 
 from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Error
 
 
-html = ''
-
-with sync_playwright() as playwright:
-    with playwright.chromium.launch(headless=False) as browser:
-        with browser.new_page() as page:
-            page.goto("https://www.linkedin.com/in/garyszekelyjr")
-            page.wait_for_load_state('load')
-            html = page.content()
-
-soup = BeautifulSoup(html, 'html.parser')
-about = soup.find('section', {'class': 'summary'}).find('p').text
-
+about = None
 experiences = [] 
-for experience in soup.find('ul', {'class': 'experience__list'}).find_all('li'):
-    title = experience.find('span', { 'class' : 'experience-item__title'}).text.strip()
-    company = experience.find('span', { 'class' : 'experience-item__subtitle'}).text.strip()
-    dates = ' - '.join(map(lambda date: date.text, experience.find_all('time')))
-
-    if '-' not in dates:
-        dates = f'{dates} - Present'
-
-    experiences.append({
-        'title': title,
-        'company': company,
-        'dates': dates
-    })
-
 educations = []
-for education in soup.find('ul', {'class': 'education__list'}).find_all('li'):
-    school = education.find('h3').text.strip()
-    degree, major = education.find('h4').text.strip().split('\n')
-    degree = degree.strip()
-    major = major.strip()
-    concentration = education.find('div', {'class': 'show-more-less-text'}).text.strip()
-    dates = ' - '.join(map(lambda date: date.text, education.find_all('time')))
-    educations.append({
-        'school': school,
-        'degree': degree,
-        'major': major,
-        'concentration': concentration,
-        'dates': dates
-    })
+data = {}
+
+while not all([about, experiences, educations]):
+    print('Scraping...')
+
+    html = ''
+
+    try:
+        with sync_playwright() as playwright:
+            with playwright.chromium.launch() as browser:
+                with browser.new_page() as page:
+                    page.goto("https://www.linkedin.com/in/garyszekelyjr")
+                    page.wait_for_load_state('load')
+                    html = page.content()
+    except Error as e:
+        print(e)
+        continue
+
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    if tag := soup.find('div', { 'class': 'core-section-container__content' }):
+        about = tag.text.strip()
+
+
+    for tag in soup.find_all('li', { 'class': 'experience-item' }):
+        experience = {}
+
+        if title := tag.find('span', { 'class': 'experience-item__title' }):
+            experience['title'] = title.text.strip()
+
+        if company := tag.find('span', { 'class': 'experience-item__subtitle' }):
+            experience['company'] = company.text.strip()
+
+        dates = []
+        for date in tag.find_all('time'):
+            dates.append(date.text.strip())
+
+        if len(dates) == 1:
+            dates.append('Present')
+
+        experience['dates'] = ' - '.join(dates)
+
+        experiences.append(experience)
+
+
+    for tag in soup.find_all('li', { 'class': 'education__list-item' }):
+        education = {}
+
+        if school := tag.find('h3'):
+            education['school'] = school.text.strip()
+
+        if degree := tag.find('h4'):
+            education['degree'], education['major'] = degree.text.strip().split('\n')
+
+        if concentration := tag.find('div', { 'class': 'show-more-less-text' }):
+            education['concentration'] = concentration.text.strip()
+
+        dates = []
+        for date in tag.find_all('time'):
+            dates.append(date.text.strip())
+
+        education['dates'] = ' - '.join(dates)
+
+        educations.append(education)
+
+
+data = {
+    'about': about,
+    'experiences': experiences,
+    'educations': educations
+}
 
 with open(Path(__file__).parent / 'assets' / 'data.json', 'w') as f:
-    json.dump({
-        'about': about,
-        'experiences': experiences,
-        'educations': educations
-    }, f)
+    json.dump(data, f, indent=8)
 
 print('Scraped!')
