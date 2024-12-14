edcb490429e635dd553c5d9cf6365eca19d9ab29
Gary Szekely <gary.szekely.jr@gmail.com>
2024-12-14 14:31:11 -0500

    updates to projects

--- src/components/Project.svelte
+++ src/components/Project.svelte
@@ -1,34 +1,78 @@
 <script lang="ts">
+	import type * as Models from "../models";
+
 	let {
 		name,
 		description,
 		html_url,
 		languages_url,
-	}: {
-		name: string;
-		description: string;
-		html_url: string;
-		languages_url: string;
-	} = $props();
+		created_at,
+		updated_at,
+	}: Models.Project = $props();
 
-	let languages: string[] = $state([]);
+	let languages: Record<string, number> | undefined = $state();
 
 	(async () => {
 		const response = await fetch(languages_url);
 		if (response.ok) {
-			languages = Object.keys(await response.json());
+			languages = await response.json();
 		}
 	})();
 </script>
 
 <div class="mx-2 my-3">
-	<a href={html_url} target="_blank" class="text-white hover:underline"
-		>{name}</a
-	>
-	<div class="text-white mb-2">{description}</div>
-	{#each languages as language}
-		<span class="mr-1 border rounded p-1 text-white"
-			>{language}</span
-		>
-	{/each}
+	<div class="flex justify-between text-white">
+		<div>
+			<a
+				href={html_url}
+				target="_blank"
+				class="text-blue-500 hover:underline">{name}</a
+			>
+			<div class="mb-2">{description}</div>
+			{#if languages}
+				<div class="flex justify-between">
+					<ul class="me-2">
+						{#each Object.keys(languages) as language}
+							<li>
+								{language}
+							</li>
+						{/each}
+					</ul>
+					<ul>
+						{#each Object.values(languages) as numBytes}
+							<li>
+								{(
+									(numBytes /
+										Object.values(
+											languages,
+										).reduce(
+											(
+												acc,
+												x,
+											) =>
+												acc +
+												x,
+											0,
+										)) *
+									100
+								).toFixed(2)}%
+							</li>
+						{/each}
+					</ul>
+				</div>
+			{/if}
+		</div>
+		<div>
+			<div class="mb-1">
+				Created: {new Date(
+					created_at,
+				).toLocaleDateString()}
+			</div>
+			<div class="mb-2">
+				Updated: {new Date(
+					updated_at,
+				).toLocaleDateString()}
+			</div>
+		</div>
+	</div>
 </div>
--- src/models.ts
+++ src/models.ts
@@ -3,6 +3,8 @@ interface Project {
 	description: string;
 	html_url: string;
 	languages_url: string;
+	created_at: string;
+	updated_at: string;
 }
 
 export type { Project };
